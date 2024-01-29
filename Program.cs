e1935f99be9418dbbb6818f8ced8d244bae5dc18
DANIL Makarenko <DanilInvest325@gmail.com>
2024-01-29 17:57:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double h = 0.2;
+            for (double x = 0; x <= 2; x += h)
+            {
+                Console.WriteLine($"y={Math.Sqrt(x) / (x + 1):f2}");
+            }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
